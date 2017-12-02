51c3ca9715b1ef290eb122b831decf0b6d5d3e71
Ziyad Edher <ziyad.edher@gmail.com>
2017-12-02 15:41:14 -0500

    Changed the way levels work

--- simulation.py
+++ simulation.py
@@ -4,19 +4,21 @@
 import time
 
 import pygame
-import pymunk
 
 
 # Screen constants
-SCREEN_SIZE = (960, 540)
+SCREEN_SIZE = (1000, 500)
 SCREEN_TITLE = "Genetic Level Solver"
 
 # Level constants
-TILE_SIZE = 96
+TILE_SIZE = 10
 
 # Color constants
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+COLORS = [BLACK, WHITE, GREEN]
 
 
 class Simulation:
@@ -38,55 +40,59 @@ class Simulation:
         pygame.display.set_caption(SCREEN_TITLE)
 
         # Initializes the level
-        self.level = Level((10, 5))
+        self.level = Level()
         self.level.draw(self.display)
 
 
 class Level:
     """Level in this simulation.
-
-    === Public Attributes ===
-    length:
-        horizontal length of this level in tiles
-    height:
-        vertical height of this level in tiles
     """
     # === Private Attributes ===
     # _grid:
     #   2-D array of level tiles
-    #   a tile is a 0 or 1 where 0 is empty and 1 is wall
+    #   a tile is a number between 0 and 2 inclusive where
+    #       0: empty
+    #       1: wall
+    #       2: point
 
-    def __init__(self, dimensions):
-        """Initializes this level with the given dimensions in the form
-        (<length>, <height>) in tiles.
+    def __init__(self):
+        """Initializes this level.
         """
-
-        # Initializes dimensions
-        self.length = dimensions[0]
-        self.height = dimensions[1]
-
         # Generates the grid
-        self._grid = self._generate_ground_grid()
+        self._grid = self._generate_boxed_grid()
 
-    def _generate_ground_grid(self):
-        """Create the tile grid to be an empty level with a ground.
-
-        Returns a 2-D array of tiles (0s or 1s).
+    def _generate_boxed_grid(self):
+        """Generates a grid with walls only at the sides.
         """
-        return [([1] + [0 for y in range(self.height - 1)])
-                for x in range(self.length)]
+        # Calculates how many rows and columns of tiles there will be
+        columns = SCREEN_SIZE[0] // TILE_SIZE
+        rows = SCREEN_SIZE[1] // TILE_SIZE
+
+        # Generates the grid and returns it
+        grid = []
+        for x in range(columns):
+            if x == 0 or x == columns - 1:
+                column = [1] * rows
+            else:
+                column = [1] + [0] * (rows - 2) + [1]
+            grid.append(column)
+        return grid
 
     def draw(self, display):
         """Draws this level to the given PyGame display.
         """
-        print(self._grid)
+        # Draws each tile in the grid
         for x in range(len(self._grid)):
             for y in range(len(self._grid[x])):
+                # Calculates the left and top
                 left = x * TILE_SIZE
-                top = SCREEN_SIZE[1] - ((y + 1) * TILE_SIZE)
-                tile_rect = pygame.Rect(left, top, TILE_SIZE, TILE_SIZE)
-                color = WHITE if self._grid[x][y] else BLACK
+                top = y * TILE_SIZE
 
+                # Creates the rectangle and chooses the color
+                tile_rect = pygame.Rect(left, top, TILE_SIZE, TILE_SIZE)
+                color = COLORS[self._grid[x][y]]
+
+                # Draws the rectangle
                 pygame.draw.rect(display, color, tile_rect)
 
 
