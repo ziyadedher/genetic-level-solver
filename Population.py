0e72f84b4ad8f100d3ecf61fb3554d6c3f4836e8
eanile <elianoanile@gmail.com>
2017-12-02 13:33:52 -0500

    Skeleton code

---
+++ Population.py
@@ -0,0 +1,13 @@
+class Population:
+
+    def __init__(self):
+        individuals = Individual[50]; # create Individual class
+
+        for individual in individuals:
+            individual = Individual();
+
+
+class Individual:
+
+    def __init__(self):
+        
