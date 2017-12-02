12aea0f5b9eeaec965da8fd018cffa2574db5ccc
eanile <elianoanile@gmail.com>
2017-12-02 14:39:30 -0500

    Added code for the individual's constructor

--- genetic_optimization.py
+++ genetic_optimization.py
@@ -1,19 +1,34 @@
+import random
+
+MAX_NODES = 5
+GENE_LENGTH = 64
+
 class Population:
 
     def __init__(self, num_individuals):
         individuals = []
 
         for i in range(num_individuals):
-             individuals[i] = Individual();
+             individuals[i] = Individual()
+
+
 
 
 class Individual:
 
     def __init__(self):
-        """initialize with random values for numNodes, numMuscles, strengths, , lengths etc."""
+        """Create a creature with random values"""
+        fitness = 0
+        genes = []
 
+        for i in range(GENE_LENGTH):
+            genes[i] = random.randint(0,1)
 
     def calculate_fitness(self):
         """create fitness function"""
 
-    def
+
+def create_new_generation():
+    """ Creates a new generation based on favourable characteristics of individuals"""
+
+
