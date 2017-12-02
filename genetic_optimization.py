6abb215d81054b46b5d96345b4479de352e05ba9
eanile <elianoanile@gmail.com>
2017-12-02 14:00:04 -0500

    More skeleton

--- genetic_optimization.py
+++ genetic_optimization.py
@@ -1,18 +1,19 @@
 class Population:
 
-    def __init__(self):
-        self.individuals = Individual[50] # create Individual class
+    def __init__(self, num_individuals):
+        individuals = []
 
-        for i in range(len(self.individuals)):
-             self.individuals[i] = Individual();
+        for i in range(num_individuals):
+             individuals[i] = Individual();
 
 
 class Individual:
 
     def __init__(self):
-        # initialize with random values for numNodes, numMuscles, strengths, , lengths etc.
-        # possibl
+        """initialize with random values for numNodes, numMuscles, strengths, , lengths etc."""
 
 
-    def find_fitness(self):
-        # create fitness function
+    def calculate_fitness(self):
+        """create fitness function"""
+
+    def
