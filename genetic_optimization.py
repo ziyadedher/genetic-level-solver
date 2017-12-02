3f447822167ab306b1f91bdbe7885cb0474fb395
eanile <elianoanile@gmail.com>
2017-12-02 18:08:06 -0500

    Improved algorithm of finding suitable parents

--- genetic_optimization.py
+++ genetic_optimization.py
@@ -6,8 +6,8 @@ DIRECTIONS = ('U', 'R', 'D', 'L')
 # Length of a creature's genes (directions)
 GENE_LENGTH = 100
 
-# Number of creatures that compete to become the parent of a new child
-NUM_TOURNAMENT_CREATURES = 5
+# Percentage of creatures to be used to create the next generation
+TOP_CREATURES_PERCENTAGE = 0.20
 
 # Threshold which determines if a random gene should be created for a child
 MUTATION_THRESHOLD = 0.08
@@ -23,32 +23,34 @@ class Population:
         """Creates a list of <num_creatures> creatures with randomly
         generated genes.
         """
-        self.population = []
+        self.pop = []
 
         for i in range(num_creatures):
-            self.population.append(Creature())
+            self.pop.append(Creature())
 
     def create_new_generation(self):
         """ Creates a new generation based on favourable characteristics
             of creatures.
         """
-        new_population = []
+        new_pop = []
+        sorted_pop = sorted(self.pop, key=get_fitness)
+        tournamet_pop = sorted_pop[0:len(sorted_pop) * TOP_CREATURES_PERCENTAGE]
 
-        for i in range(len(self.population)):
-            parent1 = find_parent(self.population)
-            parent2 = find_parent(self.population)
+        for i in range(len(self.pop)):
+            parent1 = random.choice(tournamet_pop)
+            parent2 = random.choice(tournamet_pop)
             new_child = crossover(parent1, parent2)
-            new_population.append(new_child)
+            new_pop.append(new_child)
 
-        self.population = new_population
+        self.pop = new_pop
 
     def calculate_average_fitness(self):
         """Calculates the average fitness of a generation of creatures"""
         avg_fitness = 0
-        for creature in self.population:
+        for creature in self.pop:
             avg_fitness += creature.fitness
 
-        return avg_fitness / len(self.population)
+        return avg_fitness / len(self.pop)
 
 
 class Creature:
@@ -62,25 +64,18 @@ class Creature:
             self.genes.append(random.choice(DIRECTIONS))
 
 
-def find_parent(population):
-    """Returns a parent used for creating a child."""
-    tournament_population = []
+def get_fitness(creature):
+    return creature.fitness
 
-    for i in range(NUM_TOURNAMENT_CREATURES):
-        tournament_population.append(random.choice(population))
-
-    return find_fittest(tournament_population)
-
-
-def find_fittest(population):
-    """Returns the fittest creature in a given population."""
-    fittest = population[0]
-
-    for creature in population:
-        if creature.fitness > fittest.fitness:
-            fittest = creature
-
-    return fittest
+# def find_fittest(pop):
+#     """Returns the fittest creature in a given population."""
+#     fittest = pop[0]
+#
+#     for creature in pop:
+#         if creature.fitness > fittest.fitness:
+#             fittest = creature
+#
+#     return fittest
 
 
 def crossover(parent1, parent2):
